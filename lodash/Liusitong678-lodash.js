9985d1567812a450416b315b29e296598286ae1b
sitong <sitong678@foxmail.com>
2020-06-08 17:38:23 +0800

    new works

---
+++ lodash/Liusitong678-lodash.js
@@ -0,0 +1,16 @@
+var Liusitong678 = {
+  isNull:function(val){
+    if (vall === null){
+      return ture
+    }else{
+      reutrn false
+    }
+  },
+  isNaN: function(val){
+    if (val !== val){
+      return true
+    }else{
+      return flase
+    }
+  }
+}
