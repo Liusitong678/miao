caf771b199ba738288eb6aae0235674064e29ea5
sitong <sitong678@foxmail.com>
2020-06-08 17:43:35 +0800

    change

--- lodash/Liusitong678-lodash.js
+++ lodash/Liusitong678-lodash.js
@@ -1,16 +1,16 @@
 var Liusitong678 = {
   isNull:function(val){
-    if (vall === null){
+    if (val === null){
       return ture
     }else{
-      reutrn false
+      return false
     }
   },
   isNaN: function(val){
     if (val !== val){
       return true
     }else{
-      return flase
+      return false
     }
   }
 }
